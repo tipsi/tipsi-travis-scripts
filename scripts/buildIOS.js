7c15c289619228ad0e03926c30fe391e934ed672
Igor Kozlovsky <igor.lemon.kozlovsky@gmail.com>
2019-07-24 03:50:38 +0300

    App RN build system.

--- scripts/buildIOS.js
+++ scripts/buildIOS.js
@@ -12,39 +12,44 @@ import log from './utils/log'
 
 const isProjectUsingPods = config.get('usePods')
 
-function buildIOSProject(projectPath) {
-  const iosFolder = path.resolve(projectPath, 'ios')
-  const whatToBuild = isProjectUsingPods
-    ? '-workspace example.xcworkspace'
-    : '-project example.xcodeproj'
+function buildIOSProject(projectPath, isReactNativeBuild) {
+  if (isReactNativeBuild) {
+    run('react-native run-ios --simulator="iPhone 6" --configuration=release', projectPath)
+  } else {
+    const iosFolder = path.resolve(projectPath, 'ios')
+    const whatToBuild = isProjectUsingPods
+      ? '-workspace example.xcworkspace'
+      : '-project example.xcodeproj'
 
-  log('RUN RELEASE BUILD', 'info')
-  const xcodebuildArguments = [
-    'build',
-    whatToBuild,
-    '-scheme example',
-    '-configuration Release',
-    '-sdk iphonesimulator',
-    "-destination 'platform=iOS Simulator,name=iPhone 6'",
-    '-derivedDataPath build',
-    'ONLY_ACTIVE_ARCH=NO',
-    "OTHER_LDFLAGS='$(inherited) -ObjC -lc++'",
-  ].join(' ')
+    log('RUN RELEASE BUILD', 'info')
+    const xcodebuildArguments = [
+      'build',
+      whatToBuild,
+      '-scheme example',
+      '-configuration Release',
+      '-sdk iphonesimulator',
+      "-destination 'platform=iOS Simulator,name=iPhone 6'",
+      '-derivedDataPath build',
+      'ONLY_ACTIVE_ARCH=NO',
+      '-UseModernBuildSystem=NO',
+      "OTHER_LDFLAGS='$(inherited) -ObjC -lc++'",
+    ].join(' ')
 
-  const formatter = commandExists.sync('xcpretty') ? '| xcpretty' : ''
-  run(`xcodebuild ${xcodebuildArguments} ${formatter}`, iosFolder)
+    const formatter = commandExists.sync('xcpretty') ? '| xcpretty' : ''
+    run(`xcodebuild ${xcodebuildArguments} ${formatter}`, iosFolder)
+  }
 }
 
-export default function buildIOS() {
+export default function buildIOS(isReactNativeBuild = false) {
   if (IS_MACOS && IS_IOS) {
-    log('', 'empty') // only for beautiful stdout
-    log('BUILD IOS DEFAULT')
-    buildIOSProject(DEFAULT_TESTS_FOLDER)
-
     if (isProjectUsingPods) {
       log('', 'empty') // only for beautiful stdout
       log('BUILD IOS PODSPEC')
-      buildIOSProject(PODSPEC_TESTS_FOLDER)
+      buildIOSProject(PODSPEC_TESTS_FOLDER, isReactNativeBuild)
+    } else {
+      log('', 'empty') // only for beautiful stdout
+      log('BUILD IOS DEFAULT')
+      buildIOSProject(DEFAULT_TESTS_FOLDER, isReactNativeBuild)
     }
 
     log('', 'empty') // only for beautiful stdout
